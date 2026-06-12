9479ce21c98866e3d4cebb1a1eee18b3cd9a2e7f
Christian <christianpereiradasilva2006@gmail.com>
2026-06-12 11:25:10 -0300

    feat: Estatisticas adicionadas (tempo, custo e nós), Copiar mapa para a area de transferencia

--- src/interface.py
+++ src/interface.py
@@ -1,27 +1,33 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QMainWindow, QGraphicsScene, QGraphicsView, QVBoxLayout, QHBoxLayout, QPushButton, QWidget, QLabel, QFileDialog
-from PyQt6.QtCore import Qt
-from PyQt6.QtGui import QPen, QBrush, QColor, QPainter
-from djikstra_geometrico import carregar_mapa_poly, dijkstra,construir_grafo
-from conversor_osm import parse_osm
 from math import sqrt
+
+from PyQt5.QtGui import QClipboard, QPainter
+from PyQt6.QtWidgets import QApplication, QMainWindow, QGraphicsScene, QGraphicsView, QVBoxLayout, QHBoxLayout, \
+    QPushButton, QWidget, QLabel, QFileDialog
+from PyQt6.QtCore import Qt
+
+from PyQt6.QtGui import QPen, QBrush, QColor, QImage, QPainter
+
+from djikstra_geometrico import carregar_mapa_poly, dijkstra, construir_grafo
+
 
 class Janela(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Trabalho de AED2")
+        self.setWindowTitle("Trabalho de AED2 - Caminho Mínimo")
         self.setGeometry(100, 100, 1200, 800)
 
         self.origem_selecionada = None
         self.destino_selecionado = None
-        self.estado = None
+        self.estado = "Aguardando importação"
         self.caminho_resultado = []
-        
-        arq_poly = "../out/mapaUFG.poly"
-        self.vertices, self.arestas = carregar_mapa_poly(arq_poly)
-        self.grafo = construir_grafo(self.vertices,self.arestas)
-        
-        self.mapa_vertices = {v.id: v for v in self.vertices}
+        self.resultado = None
+        self.copiado = QClipboard
+
+        self.vertices = []
+        self.arestas = []
+        self.grafo = []
+        self.mapa_vertices = {}
 
         self.init_ui()
         self.desenhar_grafo()
@@ -31,37 +37,35 @@ class Janela(QMainWindow):
         self.view = VisualizadorMapa(self.scene, self)
 
         self.painel = QVBoxLayout()
-        
-        self.lbl_titulo = QLabel("Algoritmo de Dijkstra")
-        
 
-        self.lbl_status = QLabel("Origem: Não selecionada<p>Destino: Não selecionado</p><p>Estado: Nulo</p>")
-        
-        
-        self.lbl_estatisticas = QLabel("<b>Estatísticas:</b> <p>Tempo: -</p>"
-                                       "<p>Nós explorados: -</p>"
-                                       "<p>Custo: -</p>")
+        self.lbl_titulo = QLabel("<b>Algoritmo de Dijkstra</b>")
+        self.lbl_status = QLabel(
+            "Origem: Não selecionada<p>Destino: Não selecionado</p><p>Estado: Aguardando importação</p>")
+        self.lbl_estatisticas = QLabel("<b>Estatísticas:</b><br>Tempo: -<br>Nós explorados: -<br>Custo: -")
 
-        self.btn_importar = QPushButton("Importar Mapa (.osm)")
+        self.btn_importar = QPushButton("Importar Mapa (.poly)")
         self.btn_importar.clicked.connect(self.importar_mapa)
 
         self.btn_calcular = QPushButton("Calcular Menor Caminho")
         self.btn_calcular.clicked.connect(self.calcular_caminho)
 
+        self.btn_copiar = QPushButton("Copiar Mapa (Ctrl+C)")
+        self.btn_copiar.clicked.connect(self.copiar_imagem_grafo)
+
         self.btn_limpar = QPushButton("Desfazer Seleção")
         self.btn_limpar.clicked.connect(self.limpar_selecoes)
-
 
         self.painel.addWidget(self.lbl_titulo)
         self.painel.addWidget(self.btn_importar)
         self.painel.addWidget(self.lbl_status)
+        self.painel.addWidget(self.btn_copiar)
         self.painel.addWidget(self.btn_calcular)
         self.painel.addWidget(self.btn_limpar)
         self.painel.addWidget(self.lbl_estatisticas)
         self.painel.addStretch()
 
         layout_horizontal = QHBoxLayout()
-        
+
         widget_lateral = QWidget()
         widget_lateral.setLayout(self.painel)
         layout_horizontal.addWidget(widget_lateral, stretch=2)
@@ -70,29 +74,28 @@ class Janela(QMainWindow):
         conteudo_central = QWidget()
         conteudo_central.setLayout(layout_horizontal)
         self.setCentralWidget(conteudo_central)
-    
+
     def desenhar_grafo(self):
         self.scene.clear()
 
         dir_mao_dupla = QPen(QColor("#2980b9"), 2)
-        dir_mao_unica = QPen(QColor("#16a085"), 2) 
-        
+        dir_mao_unica = QPen(QColor("#16a085"), 2)
+
         vertice_normal = QBrush(QColor("#e5e5e6"))
-        vertice_origem = QBrush(QColor("#11ff00")) 
-        vertice_destino = QBrush(QColor("#ff0000")) 
+        vertice_origem = QBrush(QColor("#11ff00"))
+        vertice_destino = QBrush(QColor("#ff0000"))
 
         for aresta in self.arestas:
             x1 = self.vertices[aresta.orig].x
             y1 = self.vertices[aresta.orig].y
-            x2 = self.vertices[aresta.dest].x 
+            x2 = self.vertices[aresta.dest].x
             y2 = self.vertices[aresta.dest].y
-            dir = dir_mao_unica if aresta.tipo == 1 else dir_mao_dupla
-            
-            self.scene.addLine(x1, y1, x2, y2, dir)
+            dir_caneta = dir_mao_unica if aresta.tipo == 1 else dir_mao_dupla
+            self.scene.addLine(x1, y1, x2, y2, dir_caneta)
 
         if self.caminho_resultado and len(self.caminho_resultado) > 1:
             pen_rota = QPen(QColor("#e67e22"), 4, Qt.PenStyle.SolidLine)
-            
+
             for i in range(len(self.caminho_resultado) - 1):
                 u = self.caminho_resultado[i]
                 v = self.caminho_resultado[i + 1]
@@ -103,22 +106,28 @@ class Janela(QMainWindow):
                 if v_u and v_v:
                     self.scene.addLine(v_u.x, v_u.y, v_v.x, v_v.y, pen_rota)
 
-        
-
         for vertice in self.vertices:
             vertice_tipo = vertice_normal
             tamanho = 10
-            
+
             if vertice.id == self.origem_selecionada:
-                vertice_tipo = vertice_origem
+                if vertice_tipo == vertice_origem:
+                    vertice_tipo = vertice_normal
+                else:
+                    vertice_tipo = vertice_origem
                 tamanho = 14
+
             elif vertice.id == self.destino_selecionado:
                 vertice_tipo = vertice_destino
                 tamanho = 14
-            
-            self.scene.addEllipse(vertice.x - tamanho/2, vertice.y - tamanho/2, tamanho, tamanho, QPen(Qt.GlobalColor.black), vertice_tipo)
+
+            self.scene.addEllipse(vertice.x - tamanho / 2, vertice.y - tamanho / 2, tamanho, tamanho,
+                                  QPen(Qt.GlobalColor.black), vertice_tipo)
 
     def tratar_clique_mapa(self, x, y):
+        if not self.vertices:
+            return
+
         proximidade_limite = 10.0
         vertice_proximo = None
         menor_distancia = float('inf')
@@ -132,50 +141,96 @@ class Janela(QMainWindow):
         if vertice_proximo is not None:
             if self.origem_selecionada is None:
                 self.origem_selecionada = vertice_proximo
+                self.estado = "Selecione o destino"
             elif self.destino_selecionado is None and vertice_proximo != self.origem_selecionada:
                 self.destino_selecionado = vertice_proximo
-            
+                self.estado = "Pronto para calcular"
+
             self.atualizar_interface_status()
             self.desenhar_grafo()
 
     def atualizar_interface_status(self):
-        origem_txt = f"<font color='green'><b>{self.origem_selecionada}</b></font>" if self.origem_selecionada else "Não selecionada"
-        destino_txt = f"<font color='red'><b>{self.destino_selecionado}</b></font>" if self.destino_selecionado else "Não selecionado"
-        self.lbl_status.setText(f"Origem: {origem_txt}<p>Destino: {destino_txt}</p><p>Estado: {self.estado}")
+        origem_txt = f"<font color='green'><b>{self.origem_selecionada}</b></font>" if self.origem_selecionada is not None else "Não selecionada"
+        destino_txt = f"<font color='red'><b>{self.destino_selecionado}</b></font>" if self.destino_selecionado is not None else "Não selecionado"
+        self.lbl_status.setText(f"Origem: {origem_txt}<p>Destino: {destino_txt}</p><p>Estado: {self.estado}</p>")
 
     def importar_mapa(self):
-        caminho_arquivo, _ = QFileDialog.getOpenFileName(self, "Selecionar Arquivo de Mapa", "", "Arquivos (*.osm)")
+        caminho_arquivo, _ = QFileDialog.getOpenFileName(self, "Selecionar Arquivo de Mapa", "", "Arquivos (*.poly)")
+        if not caminho_arquivo:
+            return
+
         print(f"Arquivo selecionado: {caminho_arquivo}")
-        parse_osm(caminho_arquivo)
+        self.vertices, self.arestas = carregar_mapa_poly(caminho_arquivo)
+        self.grafo = construir_grafo(self.vertices, self.arestas)
+
+        self.mapa_vertices = {v.id: v for v in self.vertices}
+
+        self.limpar_selecoes()
+        self.estado = "Selecione a origem"
+        self.atualizar_interface_status()
 
     def limpar_selecoes(self):
         self.origem_selecionada = None
         self.destino_selecionado = None
-        self.estado = None
-        self.atualizar_interface_status()
+        self.estado = "Selecione a origem" if self.vertices else "Aguardando importação"
         self.caminho_resultado = []
-        self.lbl_estatisticas.setText("<b>Estatísticas:</b>\nTempo: -\nNós explorados: -\nCusto: -")
-        self.lbl_status.setText(f"Origem: {self.origem_selecionada}<p>Destino: {self.destino_selecionado}</p><p>Estado: {self.estado}")
+        self.resultado = None
+        self.lbl_estatisticas.setText("<b>Estatísticas:</b><br>Tempo: -<br>Nós explorados: -<br>Custo: -")
+        self.atualizar_interface_status()
         self.desenhar_grafo()
 
     def calcular_caminho(self):
         if (self.origem_selecionada is not None) and (self.destino_selecionado is not None):
-            self.resultado = dijkstra(self.grafo,self.vertices,self.origem_selecionada,self.destino_selecionado)
+            self.resultado = dijkstra(self.grafo, self.vertices, self.origem_selecionada, self.destino_selecionado)
 
             if self.resultado is not None:
-                estatistica_teste = (f"<b>Estatísticas:</b><p>Tempo: - {self.resultado.get("tempo_ms", float)} ms</p>" +
-                                              f"<p>Nós explorados: - {self.resultado.get("nos_explorados", float)} nós</p>"
-                                              f"<p>Custo: - {self.resultado.get("distancia_metros", float):.2f} metros</p>")
-                self.estado = "Concluido"
+                tempo = self.resultado.get('tempo_ms', 0.0)
+                nos = self.resultado.get('nos_explorados', 0)
+                dist = self.resultado.get('distancia_metros', 0.0)
 
-                self.lbl_estatisticas.setText(estatistica_teste)
+                estatistica_texto = (
+                    f"<b>Estatísticas:</b>"
+                    f"<br>Tempo: {tempo:.2f} ms"
+                    f"<br>Nós explorados: {nos} nós"
+                    f"<br>Custo: {dist:.2f} metros"
+                )
+
+                self.estado = "Concluído"
+                self.lbl_estatisticas.setText(estatistica_texto)
 
                 self.caminho_resultado = self.resultado.get("caminho_ids", [])
                 self.desenhar_grafo()
             else:
                 self.estado = "Não Encontrado"
+                self.caminho_resultado = []
+                self.desenhar_grafo()
 
             self.atualizar_interface_status()
+
+    def copiar_imagem_grafo(self):
+        # Pega o conteudo até as bordas
+        area_cena = self.scene.itemsBoundingRect()
+
+        if area_cena.isEmpty():
+            self.estado = "Mapa vazio."
+            self.atualizar_interface_status()
+            return
+
+        # Pega essa cena (tela) e transforma em uma image
+        imagem = QImage(area_cena.size().toSize(), QImage.Format.Format_ARGB32)
+        # Cobre o restante da imagem com a cor branco
+        imagem.fill(Qt.GlobalColor.white)
+
+        painter = QPainter(imagem)
+        self.scene.render(painter, target=imagem.rect().toRectF(), source=area_cena)
+        painter.end()
+
+        # Copia essa imagem para a clipboard
+        clipboard = QApplication.clipboard()
+        clipboard.setImage(imagem)
+
+        self.estado = "Imagem copiada para a área de transferência!"
+        self.atualizar_interface_status()
 
 
 class VisualizadorMapa(QGraphicsView):
@@ -197,12 +252,9 @@ class VisualizadorMapa(QGraphicsView):
         else:
             super().mousePressEvent(event)
 
-    def calculo_caminho(self):
-        self.resultado = dijkstra(self.grafo,self.vertices,self.origem_selecionada,self.destino_selecionado)
-        caminho, tempo_ms, nos_explorados, dist_metros = self.resultado
-        self.caminho_resultado = caminho
 
-app = QApplication(sys.argv)
-janela = Janela()
-janela.show()
-sys.exit(app.exec())
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    janela = Janela()
+    janela.show()
+    sys.exit(app.exec())
